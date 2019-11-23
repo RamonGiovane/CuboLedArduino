3e816d870ff664352e48a42c7de1aeb10a2f66bd
leuribeiru <leuribeiru@gmail.com>
2019-11-23 15:33:50 -0300

    Funcionando versão 1
    Detalhes da implementação nos comentários do código

--- ino/cubo-led.ino
+++ ino/cubo-led.ino
@@ -1,6 +1,10 @@
 int colunas[] = {10, 9, 8, 7, 6, 5, 4, 3, 2};   
 int linhas[] = {13, 12, 11};
-int tam = 5;
+
+int matriz[3][9];
+
+int strSize = 5;
+char str[5];
    
 void setup(){
   for (int contador = 0; contador < 9; contador++){
@@ -9,30 +13,90 @@ void setup(){
   for (int contador = 0; contador < 3; contador++){
       pinMode(linhas[contador], OUTPUT);
   }
+
+  //marca todos os leds como desligados
+  for(int lin=0; lin<3; lin++){
+    for(int col=1; col<9; col++){
+      matriz[lin][col] = 0;
+    }
+  }
+  
+  Serial.begin(9600);
+  
 }
    
 void loop(){
-  printReceivedData();
+  
+  //recebe um dado no formato acção;linha;coluna
+  //ações: l -> adiciona, f->remove
+  //linha: valores-> 0,1 ou 2
+  //coluna: valores-> 0.1.2.3.4.5.6.7 ou 8
+  //exemplo: l;2;2 l;2;6  f;2;2 
+  
+  lerSerial();
+
+  //percorre a matriz e acende os leds com valor = 1
+  for(int lin=0; lin<3; lin++){
+    for(int col=0; col<9; col++){
+      if(matriz[lin][col] == 1){
+        acendeLed(lin, col);
+        apagaLed(lin, col);
+      }
+    }
+  }
   
 }
 
-void acenderLed(int linha, int coluna){
-    digitalWrite(colunas[coluna-1], HIGH);
-    digitalWrite(linhas[linha-1], HIGH);
-}
-
-void apagarLed(int linha, int coluna){
-    digitalWrite(colunas[coluna-1], LOW);
-    digitalWrite(linhas[linha-1], LOW);
-}
-
-String printReceivedData(){
-    char str[tam]; /// L;X;Y
+//lê o conteúdo da porta serial e encaminha para a ação correta
+void lerSerial(){
+  int tam = Serial.readBytes(str, strSize);
+  if(tam > 0){
     
-    int bytes = Serial.readBytes(str, tam);
- 
-    if(bytes > 0)
-            Serial.print(str);
-            Serial.print('\n');
-    } 
+    //adiciona o valor 1 para o led na matriz
+    if(str[0] == 'l'){
+      adicionaLed(str[2]-48,str[4]-48);
+    }
+    //adiciona o valor 0 para o led na matriz
+    if(str[0] == 'f'){
+      removeLed(str[2],str[4]-48);
+    }
+  }
+
+}
+
+
+void acendeLed(int linha, int coluna){
+    digitalWrite(colunas[coluna], HIGH);
+    digitalWrite(linhas[linha], HIGH);
+}
+
+void apagaLed(int linha, int coluna){
+    digitalWrite(colunas[coluna], LOW);
+    digitalWrite(linhas[linha], LOW);
+}
+
+void adicionaLed(int linha, int coluna){
+  matriz[linha][coluna] = 1;
+  /*
+  Serial.print("Adicionou ");
+  Serial.print(linha);
+  Serial.print(" ");
+  Serial.print(coluna);
+  Serial.print(" = ");
+  Serial.print(matriz[linha][coluna]);
+  Serial.print("\n");
+  */
+}
+
+void removeLed(int linha, int coluna){
+  matriz[linha][coluna] = 0; 
+  /*
+  Serial.print("Removeu ");
+  Serial.print(linha);
+  Serial.print(" ");
+  Serial.print(coluna);
+  Serial.print(" = ");
+  Serial.print(matriz[linha][coluna]);
+  Serial.print("\n");
+  */
 }
